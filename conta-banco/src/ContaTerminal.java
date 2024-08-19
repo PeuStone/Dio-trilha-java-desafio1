3f2e9c9c1f33eb44f6b6da4f2074cac7d592a843
PeuStone <pedrohmotaaa@gmail.com>
2024-08-19 10:38:56 -0300

    Criação da lógica por trás do projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Scanner scanner  = new Scanner(System.in);
+
+        System.out.println("Digite o número de sua conta ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Agora digite o número da agência");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o nome do cliente");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o sobrenome do cliente");
+        String sobrenomeCliente = scanner.next();
+
+        System.out.println("Digite o saldo atual da conta");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " " + sobrenomeCliente + ", obrigado por criar uma conta em nosso banco," +
+                " sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo de " + saldo +
+                " já está disponível para saque");
     }
 }
